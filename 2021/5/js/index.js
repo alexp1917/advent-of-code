730faf2546970d0aa2f19c69ce8fc9709ac68c8b
Alex Pavlovich <alexpimt1917@gmail.com>
2021-12-12 20:01:15 -0500

    2021 N5 P1 works

---
+++ 2021/5/js/index.js
@@ -0,0 +1,105 @@
+function main(lines) {
+  var state = getInitialState(1000);
+  for (var line of lines) {
+    var ventCoordinate = parse(line);
+    var aligned = isAligned(ventCoordinate);
+
+    /**
+     * if (aligned) console.log(ventCoordinate)
+     *
+     * [ [ 0, 9 ], [ 5, 9 ] ]
+     * [ [ 9, 4 ], [ 3, 4 ] ]
+     * [ [ 2, 2 ], [ 2, 1 ] ]
+     * [ [ 7, 0 ], [ 7, 4 ] ]
+     * [ [ 0, 9 ], [ 2, 9 ] ]
+     * [ [ 3, 4 ], [ 1, 4 ] ]
+     * [Finished in 63ms]
+     */
+    if (aligned)
+      mark(ventCoordinate, state);
+  }
+
+  console.log(formatMatrix(state.matrix))
+  var numOverlaps = countOverlaps(state.matrix);
+  console.log('we have', numOverlaps, 'places where vents overlap')
+}
+
+function countOverlaps(matrix) {
+  var result = 0;
+  for (var i = 0; i < matrix.length; i++) {
+    var row = matrix[i];
+
+    for (var j = 0; j < row.length; j++) {
+      var element = row[j];
+
+      if (element.vents > 1)
+        result++;
+    }
+  }
+  return result;
+}
+
+function formatMatrix(matrix) {
+  return matrix.map(row => (
+    row.map(el => el.vents || '*').join('')
+  )).join(os.EOL);
+}
+
+function getInitialState(matrixSize = 9) {
+  return {
+    matrix: new Array(matrixSize).fill(null)
+      .map(() => new Array(matrixSize).fill(null)
+        .map(() => ({ vents: 0 }))),
+  };
+}
+
+function mark(coord, state) {
+  var [ [ x1, y1 ], [ x2, y2 ] ] = coord;
+  var { matrix } = state;
+
+  var x_min = Math.min(x1, x2);
+  var x_max = Math.max(x1, x2);
+  var y_min = Math.min(y1, y2);
+  var y_max = Math.max(y1, y2);
+
+  if (y1 === y2) {
+    // console.log('marching from x1 to x2', x1, x2, 'at', y1);
+
+    for (var x = x_min; x <= x_max; x++) {
+      matrix[y1][x].vents++;
+    }
+  } else if (x1 === x2) {
+    // console.log('marching from y1 to y2', y1, y2, 'at', x1);
+
+    for (var y = y_min; y <= y_max; y++) {
+      matrix[y][x1].vents++;
+    }
+  }
+
+  // console.log('after coord', coord, 'matrix is now')
+  // console.log(formatMatrix(state.matrix))
+}
+
+function vertical(c) {
+  return c[0][0] === c[1][0]
+}
+
+function horizontal(c) {
+  return c[0][1] === c[1][1];
+}
+
+function isAligned(c) {
+  return horizontal(c) || vertical(c);
+}
+
+function parse(l) {
+  return l.split(' -> ').map(e => {
+    return e.split(',').map(n => parseInt(n, 10));
+  });
+}
+
+var fs = require('fs');
+var os = require('os');
+
+// main(fs.readFileSync('sample-input1.txt', 'utf8').split(os.EOL));
+main(fs.readFileSync('input1.txt', 'utf8').split(os.EOL));
